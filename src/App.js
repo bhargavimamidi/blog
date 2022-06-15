0a17e3787913451b2f019fdf40715c1431540810
bhargavimamidi <saibhargavi260@gmail.com>
2022-06-15 13:06:08 +0530

    Topbar added

--- src/App.js
+++ src/App.js
@@ -3,22 +3,7 @@ import TopBar from "./components/topbar/TopBar";
 
 function App() {
   return (
-    <div>
-      <div className="topLeft"> 
-      <i class="fa-brands fa-instagram"></i>
-          <i class="fa-brands fa-whatsapp"></i>
-          <i class="fa-brands fa-twitter"></i>
-          <i class="fa-brands fa-facebook"></i>
-      </div>
-      <div className="topCenter">
-        <ul>
-          <li>
-            H 
-          </li>
-        </ul>
-       </div>
-      <div className="topRight"> R </div>
-    </div>
+   <TopBar />
     
   );
 }
--- src/components/topbar/TopBar.js
+++ src/components/topbar/TopBar.js
@@ -3,7 +3,26 @@ import './topbar.css';
 export default function TopBar(){
     return (
         <div className='top'>
-            TopBar
+            <div className="topLeft"> 
+      <i class="fa-brands fa-instagram"></i>
+          <i class="fa-brands fa-whatsapp"></i>
+          <i class="fa-brands fa-twitter"></i>
+          <i class="fa-brands fa-facebook"></i>
+      </div>
+      <div className="topCenter">
+      <ul className="topList">
+          <li className="topListItem">
+              HOME
+          </li>
+          <li className="topListItem">ABOUT</li>
+          <li className="topListItem">CONTACT</li>
+          <li className="topListItem">
+              WRITE
+          </li>
+          <li className="topListItem">LOGOUT</li>o
+        </ul>
+       </div>
+      <div className="topRight"> R </div>
         </div>
     )
 }
